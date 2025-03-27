db6045093208c8eac2346b46b0fd0b38ded436d3
Durowade Abdulmajeed <durowade@hasob.ng>
2025-03-27 09:56:01 +0100

    base form request

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Foundation\Http\FormRequest;
+
+abstract class BaseFormRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    abstract public function rules();
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    abstract public function authorize();
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        if ($this->wantsJson()) {
+            $response = response()->json([
+                'errors' => $validator->errors()
+            ]);
+        } else {
+
+            $response = redirect()
+                ->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        throw (new ValidationException($validator, $response))
+            ->errorBag($this->errorBag)
+            ->redirectTo($this->getRedirectUrl());
+    }
+}
